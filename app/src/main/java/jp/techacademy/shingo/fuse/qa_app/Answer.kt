23f79ac92b25735073912731487f61ccea65ac70
shingo-fuse <1.useful.app@gmail.com>
2023-06-07 11:59:50 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/shingo/fuse/qa_app/Answer.kt
@@ -0,0 +1,7 @@
+package jp.techacademy.shingo.fuse.qa_app
+
+
+import java.io.Serializable
+
+class Answer(val body: String, val name: String, val uid: String, val answerUid: String) :
+    Serializable {}
---
+++ app/src/main/java/jp/techacademy/shingo/fuse/qa_app/Const.kt
@@ -0,0 +1,13 @@
+package jp.techacademy.shingo.fuse.qa_app
+
+//Firebaseにユーザーの表示名を保存するパス
+const val UsersPATH ="users"
+
+//FireBaseに質問を保存するパス
+const val ContentsPATH ="contents"
+
+//Firebaseに保存するパス
+const val AnswersPATH = "answers"
+
+//preferenceに表示名を保存する時のキー
+const val NameKEY = "name"
